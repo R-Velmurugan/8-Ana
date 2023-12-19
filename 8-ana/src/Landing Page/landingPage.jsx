0787fe6f459b8aec2fe6b9e051f2264a2527f188
Velmurugan <vel@Velmurugans-MacBook-Pro.local>
2023-12-19 19:37:20 +0530

    added the name and hero-image with blob background

--- 8-ana/src/Landing Page/landingPage.jsx
+++ 8-ana/src/Landing Page/landingPage.jsx
@@ -4,7 +4,14 @@ function LandingPage(){
     const rootElement = document.getElementById("root");
 
     return(
-        <div className = "backgroundImage">
+        <div className = "landing-page">
+            <div className = "logo-name">
+                <h1>8 Ana</h1>
+                <h1>Expense tracker</h1>
+            </div>
+            <div className = "hero-image-wrapper">
+                <img className="hero-image" src={"/images/expense-tracker-0-bg.png"} alt="" />
+            </div>
         </div>
     );
 }
